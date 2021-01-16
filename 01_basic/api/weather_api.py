b230d9220c2cf92d45d7ce2f947a8504558ca07e
victordomingos <editor.arcosonline@gmail.com>
2021-01-16 15:22:26 +0000

    validation

--- 01_basic/api/weather_api.py
+++ 01_basic/api/weather_api.py
@@ -4,6 +4,7 @@ import fastapi
 from fastapi import Depends
 
 from models.location import Location
+from models.validation_error import ValidationError
 from services.openweather_service import get_report
 
 router = fastapi.APIRouter()
@@ -15,4 +16,7 @@ async def weather(location: Location = Depends(), units: Optional[str] = 'metric
     Example:
         /api/weather/Braga?country=PT&units=metric
     """
-    return await get_report(location.city, location.country, units)
+    try:
+        return await get_report(location.city, location.country, units)
+    except ValidationError as ex:
+        return fastapi.Response(content=ex.error_msg, status_code=ex.status_code)
--- 01_basic/services/openweather_service.py
+++ 01_basic/services/openweather_service.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
 import httpx
+from httpx import Response
 
 from api import weather_cache
 from api.weather_cache import get_weather
+from models.validation_error import ValidationError
 
 api_key: Optional[str] = None
 
@@ -16,8 +18,9 @@ async def get_report(city: str, country: str, units: str) -> dict:
     url = f'http://api.openweathermap.org/data/2.5/weather?q={query}&appid={api_key}&units={units}'
 
     async with httpx.AsyncClient() as client:
-        r = await client.get(url)
-        r.raise_for_status()
+        r: Response = await client.get(url)
+        if r.status_code != 200:
+            raise ValidationError(r.text, status_code=r.status_code)
 
     data = r.json()
     forecast = data['main']
